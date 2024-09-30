3922709dc7a112c13f1cc8ad70b2e2c5aa602740
Patrick Conroy <patrick@conroy-family.net>
2024-09-30 16:49:58 -0600

    trying out the findController call in the lib now

--- main.c
+++ main.c
@@ -20,18 +20,27 @@
  */
 int main(int argc, char** argv) 
 {
-    const char *devPort = "/dev/ttyXRUSB0";
+    char    *controllerPortName = NULL;
     
     Logger_Initialize( "/tmp/epsolar_syncclock.log", 5 );
-    Logger_LogWarning( "Version: %s\n", "v1.0"  );
+    Logger_LogWarning( "Version: %s\n", "v2.0"  );
+    
+    controllerPortName = findController( "/dev/ttyACM", 10, TRUE );
+    if (controllerPortName != NULL) {
+        Logger_LogInfo( "find controller worked - setting real time clock and disconnecting\n" );
+        eps_setRealtimeClockToNow();
+        epsolarModbusDisconnect();
+    } else {
+        Logger_LogInfo( "find controller DID NOT WORK - trying /dev/ttyACM1\n" );
+    
+        if (!epsolarModbusConnect( "/dev/ttyACM1", 1 )) {
+            Logger_LogFatal( "Unable to open %s to connect to the solar charge controller\n", devPort );
+            return (EXIT_FAILURE);
+        }
 
-    if (!epsolarModbusConnect( devPort, 1 )) {
-        Logger_LogFatal( "Unable to open %s to connect to the solar charge controller\n", devPort );
-        return (EXIT_FAILURE);
+        eps_setRealtimeClockToNow();
+        epsolarModbusDisconnect();
     }
-
-    eps_setRealtimeClockToNow();
-    epsolarModbusDisconnect();
     
     Logger_LogWarning( "Clock sync'd\n");
     Logger_Terminate();
